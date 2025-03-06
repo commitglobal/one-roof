109a8aee3ed11e35418d989d4801e36511591e57
Andrei Ioniță <hi@andrei.io>
2025-03-06 19:13:15 +0100

    trust proxies

---
+++ app/Http/Middleware/SetCloudfrontHeaders.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SetCloudfrontHeaders
+{
+    protected string $header = 'Cloudfront-Forwarded-Proto';
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->hasHeader($this->header)) {
+            $request->headers->add([
+                'X-Forwarded-Proto' => $request->header($this->header),
+                'X-Forwarded-Port' => '443',
+            ]);
+        }
+
+        return $next($request);
+    }
+}
