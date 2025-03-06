4781a3c5c4d779ea4956df7f90868a002c6272ce
Andrei Ioniță <hi@andrei.io>
2025-03-06 19:22:52 +0100

    add sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use App\Http\Middleware\SetCloudfrontHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +19,5 @@ return Application::configure(basePath: dirname(__DIR__))
             ->append(SetCloudfrontHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        Integration::handles($exceptions);
     })->create();
