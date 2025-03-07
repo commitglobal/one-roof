f72998f4c633a554cd52e4c30d8d3b6a86eaf1e6
Andrei Ioniță <hi@andrei.io>
2025-03-07 18:53:52 +0000

    add spanish

---
+++ lang/vendor/table-repeater/es/components.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'repeater' => [
+        'empty' => [
+            'label' => 'No se encontraron registros',
+        ],
+        'row_actions' => [
+            'label' => 'Acciones de fila',
+        ],
+    ],
+];
