2dc3f38084e98d27769d68070aefe9fc059a9bf2
Weblate <noreply@weblate.org>
2025-03-08 17:45:01 +0000

    Added translation using Weblate (Spanish)
    Update translation files
    
    Updated by "Remove blank strings" add-on in Weblate.
    
    Added translation using Weblate (Spanish)
    
    Added translation using Weblate (Spanish)
    
    Update translation files
    
    Updated by "Cleanup translation files" add-on in Weblate.
    
    Co-authored-by: Andrei Ioniță <andrei.ionita@commitglobal.org>
    Co-authored-by: Languages add-on <noreply-addon-languages@weblate.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.commitglobal.org/projects/oneroof/app/
    Translate-URL: https://translate.commitglobal.org/projects/oneroof/validation/
    Translation: OneRoof/app
    Translation: OneRoof/validation

---
+++ lang/es/actions.php
@@ -0,0 +1,37 @@
+<?php
+return [
+    'merge' => [
+        'single' => [
+            'label' => '',
+            'modal' => [
+                'heading' => '',
+                'actions' => [
+                    'merge' => [
+                        'label' => '',
+                    ],
+                ],
+            ],
+            'notifications' => [
+                'merged' => [
+                    'title' => '',
+                ],
+            ],
+        ],
+        'multiple' => [
+            'label' => '',
+            'modal' => [
+                'heading' => '',
+                'actions' => [
+                    'merge' => [
+                        'label' => '',
+                    ],
+                ],
+            ],
+            'notifications' => [
+                'merged' => [
+                    'title' => '',
+                ],
+            ],
+        ],
+    ],
+];
---
+++ lang/es/email.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'greeting' => '',
+    'welcome' => [
+        'subject' => '',
+        'organization' => [
+            //
+            'line_1' => '',
+            'line_2' => '',
+            'line_3' => '',
+            'line_4' => '',
+            'action' => '',
+        ],
+    ],
+];
