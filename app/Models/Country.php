23b8eb8e07e8fd31bd8f06c1676fcc81ab2882ec
Andrei Ioniță <hi@andrei.io>
2025-03-11 10:53:03 +0000

    fix

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use App\Concerns\LogsActivity;
 use App\Models\Scopes\OrderbyTranslated;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
@@ -12,7 +11,6 @@ use Spatie\Translatable\HasTranslations;
 class Country extends Model
 {
     use HasTranslations;
-    use LogsActivity;
 
     public $incrementing = false;
 
