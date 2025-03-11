07067f0e6f7c6155be33f07956662188d590e711
Weblate <noreply@weblate.org>
2025-03-11 10:53:16 +0000

    Update translation files
    Updated by "Squash Git commits" add-on in Weblate.
    
    Translated using Weblate (Spanish)
    
    Currently translated at 100.0% (9 of 9 strings)
    
    Update translation files
    
    Updated by "Squash Git commits" add-on in Weblate.
    
    Co-authored-by: Olivia Vereha <olivia.vereha@commitglobal.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.commitglobal.org/projects/oneroof/actions/
    Translate-URL: https://translate.commitglobal.org/projects/oneroof/auth/
    Translate-URL: https://translate.commitglobal.org/projects/oneroof/auth/es/
    Translation: OneRoof/actions
    Translation: OneRoof/auth

---
+++ lang/es/actions.php
@@ -0,0 +1,37 @@
+<?php
+return [
+    'merge' => [
+        'single' => [
+            'label' => 'Fusionar',
+            'modal' => [
+                'heading' => 'Fusionar :label',
+                'actions' => [
+                    'merge' => [
+                        'label' => 'Fusionar',
+                    ],
+                ],
+            ],
+            'notifications' => [
+                'merged' => [
+                    'title' => 'Fusionado',
+                ],
+            ],
+        ],
+        'multiple' => [
+            'label' => 'Fusionar seleccionados',
+            'modal' => [
+                'heading' => 'Fusionar seleccionados :label',
+                'actions' => [
+                    'merge' => [
+                        'label' => 'Fusionar',
+                    ],
+                ],
+            ],
+            'notifications' => [
+                'merged' => [
+                    'title' => 'Fusionado',
+                ],
+            ],
+        ],
+    ],
+];
--- lang/es/auth.php
+++ lang/es/auth.php
@@ -1,9 +1,14 @@
 <?php
-
-declare(strict_types=1);
-
 return [
     'failed' => 'Estas credenciales no coinciden con nuestros registros.',
     'password' => 'La contraseña es incorrecta.',
     'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
+    'invalid_signature' => 'Este enlace no tiene una firma válida.',
+    'set_password' => 'Establecer contraseña',
+    'link_already_used' => 'Este enlace ya ha sido utilizado.',
+    'invalid_user' => 'Este no es un usuario válido.',
+    'suspended' => [
+        'title' => 'Tu cuenta está suspendida',
+        'body' => 'Para obtener más información, ponte en contacto con un administrador.',
+    ],
 ];
