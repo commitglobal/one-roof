8daeaa5e897c31086df6c8c742ad2615df77c705
Andrei Ioniță <hi@andrei.io>
2025-03-11 17:09:42 +0000

    fix emails

--- lang/en/email.php
+++ lang/en/email.php
@@ -9,7 +9,11 @@ return [
         'subject' => 'Welcome to OneRoof',
 
         'admin' => [
-            //
+            'line_1' => 'Welcome to OneRoof! You have been invited to join as an admin user.',
+            'line_2' => 'To set up your admin account, please follow these steps:',
+            'line_3' => '1. Click the button below to accept the invitation and create your password.',
+            'line_4' => '2. Once your password is set, log in using your registered email and new password at the link provided below.',
+            'action' => 'Accept Invitation',
         ],
 
         'organization' => [
