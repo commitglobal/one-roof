b1cf9ece8632aa9f4267d24ab298bb1753584091
Andrei Ioniță <hi@andrei.io>
2025-03-14 16:24:44 +0000

    wip

--- app/Filament/Shelter/Resources/BeneficiaryResource/Pages/ListBeneficiaries.php
+++ app/Filament/Shelter/Resources/BeneficiaryResource/Pages/ListBeneficiaries.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App\Filament\Shelter\Resources\BeneficiaryResource\Pages;
 
+use App\Filament\Concerns\DisablesBreadcrumbs;
 use App\Filament\Shelter\Resources\BeneficiaryResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListBeneficiaries extends ListRecords
 {
+    use DisablesBreadcrumbs;
+
     protected static string $resource = BeneficiaryResource::class;
 
     protected function getHeaderActions(): array
--- app/Filament/Shelter/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Shelter/Resources/UserResource/Pages/ListUsers.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App\Filament\Shelter\Resources\UserResource\Pages;
 
+use App\Filament\Concerns\DisablesBreadcrumbs;
 use App\Filament\Shelter\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListUsers extends ListRecords
 {
+    use DisablesBreadcrumbs;
+
     protected static string $resource = UserResource::class;
 
     protected function getHeaderActions(): array
