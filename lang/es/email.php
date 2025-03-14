db92f9d645025d8f15daebf3472b5ee0582e5607
Weblate <noreply@weblate.org>
2025-03-14 14:17:47 +0000

    Update translation files
    Updated by "Squash Git commits" add-on in Weblate.
    
    Translated using Weblate (Spanish)
    
    Currently translated at 75.0% (9 of 12 strings)
    
    Co-authored-by: Olivia Vereha <olivia.vereha@commitglobal.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.commitglobal.org/projects/oneroof/email/
    Translate-URL: https://translate.commitglobal.org/projects/oneroof/email/es/
    Translation: OneRoof/email

--- lang/es/email.php
+++ lang/es/email.php
@@ -11,5 +11,12 @@ return [
             'line_4' => '2. Una vez que hayas establecido tu contraseña, inicia sesión usando tu correo electrónico registrado y tu nueva contraseña en el enlace proporcionado a continuación.',
             'action' => 'Aceptar invitación',
         ],
+        'admin' => [
+            'line_2' => 'Para configurar tu cuenta de administrador, por favor sigue estos pasos:',
+            'line_3' => '1. Haz clic en el botón de abajo para aceptar la invitación y crear tu contraseña.',
+            'action' => 'Aceptar invitación',
+            'line_1' => '¡Bienvenido a UnTecho! Se le ha invitado a unirse como usuario administrador.',
+            'line_4' => '2. Una vez que hayas establecido tu contraseña, inicia sesión usando tu correo electrónico registrado y tu nueva contraseña en el enlace proporcionado a continuación.',
+        ],
     ],
 ];
