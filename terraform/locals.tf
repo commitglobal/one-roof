4660daf5574091707957d12f9507eb47a4442c44
Andrei Ioniță <hi@andrei.io>
2025-03-19 15:47:44 +0000

    infra: version bump

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,7 +2,7 @@ locals {
   namespace = "one-roof-${var.env}"
   image = {
     repo = "commitglobal/one-roof",
-    tag  = "0.2.5"
+    tag  = "0.3.0"
   }
 
   availability_zone = data.aws_availability_zones.current.names[0]
