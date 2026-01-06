df4747ab13f8dccb555f9a39533aaedc8aaf75b7
Andrei Ioniță <hi@andrei.io>
2026-01-06 16:42:27 +0200

    infra: fix ecs security group

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -41,8 +41,8 @@ resource "aws_security_group" "ecs" {
 
   ingress {
     description     = "Load balancer traffic"
-    from_port       = 80
-    to_port         = 80
+    from_port       = 8080
+    to_port         = 8080
     protocol        = "tcp"
     security_groups = [aws_security_group.lb.id]
   }
